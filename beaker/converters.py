a06f975753d2e27837deb871747c0163b9f32b7a
Marcel Rodrigues <marcelgmr@gmail.com>
2013-07-17 22:59:17 -0300

    Remove util dependence from converters.py.

--- beaker/converters.py
+++ beaker/converters.py
@@ -1,9 +1,12 @@
-import beaker.util as util
+if hasattr(__builtins__, 'unicode'):
+    text_type = unicode
+else:
+    text_type = str
 
 # (c) 2005 Ian Bicking and contributors; written for Paste (http://pythonpaste.org)
 # Licensed under the MIT license: http://www.opensource.org/licenses/mit-license.php
 def asbool(obj):
-    if isinstance(obj, util.string_types):
+    if isinstance(obj, text_type):
         obj = obj.strip().lower()
         if obj in ['true', 'yes', 'on', 'y', 't', '1']:
             return True
@@ -16,7 +19,7 @@ def asbool(obj):
 
 
 def aslist(obj, sep=None, strip=True):
-    if isinstance(obj, util.string_types):
+    if isinstance(obj, text_type):
         lst = obj.split(sep)
         if strip:
             lst = [v.strip() for v in lst]
